4d2d7e5011039ee014ea3de4dba58c46813b75aa
Lillie McAlister <lilliemcalister1993@live.com>
2023-05-25 01:21:10 -0700

    Added Movie Adapter and added to MoviesItem Class

---
+++ app/src/main/java/com/msla_mac/lam_omdb_api_final/MoviesAdapter.kt
@@ -0,0 +1,51 @@
+package com.msla_mac.lam_omdb_api_final
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+
+internal class MoviesAdapter(
+    private var moviesList : List<MoviesItem>,
+    private val listener: (position: Int) -> Unit
+    ): RecyclerView.Adapter<MoviesAdapter.MyViewHolder>() {
+
+    // TODO: finish adapter
+    inner class MyViewHolder(view: View) : RecyclerView.ViewHolder(view),
+        View.OnClickListener {
+        var title : TextView = view.findViewById(R.id.movieTitleText)
+        var year: TextView = view.findViewById(R.id.movieYearText)
+        var imdbID: TextView = view.findViewById(R.id.imbdIDText)
+        var type: TextView = view.findViewById(R.id.showTypeText)
+
+        init {
+            itemView.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View){
+            listener(adapterPosition)
+        }
+    }
+
+    override fun onCreateViewHolder(parent : ViewGroup, viewType : Int) : MyViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.movie_item, parent, false)
+        return MyViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+
+        //get the data from the list and fill in the various CELL from fields
+        val movieItem = moviesList[position]
+
+        holder.title.text = movieItem.Title
+        holder.year.text = movieItem.Year.toString()
+        holder.imdbID.text = movieItem.imdbID
+        holder.type.text = movieItem.Type
+    }
+
+    override fun getItemCount(): Int {
+        return moviesList.size
+    }
+}
--- app/src/main/java/com/msla_mac/lam_omdb_api_final/MoviesItem.kt
+++ app/src/main/java/com/msla_mac/lam_omdb_api_final/MoviesItem.kt
@@ -1,12 +1,12 @@
 package com.msla_mac.lam_omdb_api_final
 
-class MoviesItem (var Title: String, var Year: Int, var imdbID: String, var Type: String) {
+class MoviesItem (var Title: String, var Year: Int, var imdbID: String, var Type: String, var Released: String, var Director: String, var Actor: String, var Awards: String, var Poster: String, var Plot: String, var Rated: String) {
 
     fun toCSV(): String {
-        return "$Title, $Year, $imdbID, $Type"
+        return "$Title, $Year, $imdbID, $Type, $Released, $Director, $Actor, $Awards, $Poster, $Plot, $Rated"
     }
 
     override fun toString(): String {
-        return "$Title : $Year $imdbID $Type"
+        return "$Title : $Year $imdbID $Type, $Released, $Director, $Actor, $Awards, $Poster, $Plot, $Rated"
     }
 }
